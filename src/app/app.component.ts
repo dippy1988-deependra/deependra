562f95a988c5603508d863f72ce8752c8c9cd21c
Wiselysoft-2 <wiselysoft-2@Wiselysoft-2s-MacBook-Pro.local>
2017-09-16 16:43:16 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent {
   title = 'app works!';
+  items:any=[];
+  sidebar_menu:any=[];
+
+  constructor(){}
+  ngOnInit(){
+    this.items=[
+      {name:'Nitin',address:'Gaziabad'},
+      {name:'Deependra',address:'gaziabad'},
+      {name:'Gaurav',address:'gaziabad'},
+      {name:'Jitender',address:'gaziabad'},
+      {name:'Nitin',address:'Gaziabad'},
+      {name:'Deependra',address:'gaziabad'},
+      {name:'Gaurav',address:'gaziabad'},
+      {name:'Jitender',address:'gaziabad'},
+      {name:'nitin',address:'gaziabad'}
+    ];
+    this.sidebar_menu=[
+      {menu:'DASHBOARD'},
+      {menu:'REPORTS'},
+      {menu:'APPUSER'},
+      {menu:'ADMINISTRATION'},
+      {menu:'BROADCAST MESSAGE'},
+      {menu:'LOG'},
+      {menu:'VERIFICATION REQUEST'},
+      {menu:'LOGOUT'}
+    ]
+  }
+  myEvent(event) {
+    alert('working');
+  }
 }
